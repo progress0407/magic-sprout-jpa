268f6e412e2eb68bd438a9ddc924b9584651b630
progress0407 <progress0407@gmail.com>
2023-08-03 20:20:05 +0900

    Spring 의존성 제거

--- src/main/java/philo/magicsproutjpa/business/item/ItemRepository.java
+++ src/main/java/philo/magicsproutjpa/business/item/ItemRepository.java
@@ -1,11 +1,7 @@
 package philo.magicsproutjpa.business.item;
 
-import java.util.List;
 import philo.magicsproutjpa.core.MimicJpaRepository;
 
 public class ItemRepository extends MimicJpaRepository<Item, Long> {
 
-  public List<Item> findByName(String name) {
-    return invokeQueryMethod(name);
-  }
 }
